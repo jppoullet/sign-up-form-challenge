3ec1687021680248bbcec4486580155aecaee36e
jppoullet <jppoullet@yahoo.com>
2023-02-01 06:26:17 -0500

    Javascript updated with forEach

--- script.js
+++ script.js
@@ -1,46 +1,76 @@
 "use strict";
 
-const inputFirstName = document.querySelector("input[name='firstName']");
-const inputLastName = document.querySelector("input[name='lastName']");
-const inputEmail = document.querySelector("input[name='email']");
-const inputPassword = document.querySelector("input[name='password']");
+const inputs = ["firstName", "lastName", "email", "password"];
 
-inputFirstName.addEventListener("invalid", function (event) {
-  if (event.target.validity.valueMissing) {
-    document.querySelector('label[for="firstName"]').style.display = "flex";
-  }
+// Adding Event Listeners for change
+inputs.forEach((inputName) => {
+  document
+    .querySelector(`input[name=${inputName}]`)
+    .addEventListener("change", function (event) {
+      const label = document.querySelector(`label[for=${inputName}]`);
+
+      label.style.display = "none";
+    });
 });
 
-inputFirstName.addEventListener("change", function (event) {
-  document.querySelector("label[for='firstName']").style.display = "none";
+// Adding event listeners for invalid
+inputs.forEach((inputName) => {
+  document
+    .querySelector(`input[name=${inputName}]`)
+    .addEventListener("invalid", function (event) {
+      const label = document.querySelector(`label[for=${inputName}]`);
+
+      if (event.target.validity.valueMissing) {
+        label.style.display = "flex";
+      }
+    });
 });
 
-inputLastName.addEventListener("invalid", function (event) {
-  if (event.target.validity.valueMissing) {
-    document.querySelector('label[for="lastName"]').style.display = "flex";
-  }
-});
+// Instead of having two seperate forEach(), you can just put both inside one forEach()
 
-inputLastName.addEventListener("change", function (event) {
-  document.querySelector("label[for='lastName']").style.display = "none";
-});
+// My original code below
 
-inputEmail.addEventListener("invalid", function (event) {
-  if (event.target.validity.valueMissing) {
-    document.querySelector('label[for="email"]').style.display = "flex";
-  }
-});
+// const inputFirstName = document.querySelector("input[name='firstName']");
+// const inputLastName = document.querySelector("input[name='lastName']");
+// const inputEmail = document.querySelector("input[name='email']");
+// const inputPassword = document.querySelector("input[name='password']");
 
-inputEmail.addEventListener("valid", function (event) {
-  document.querySelector("label[for='email']").style.display = "none";
-});
+// inputFirstName.addEventListener("invalid", function (event) {
+//   if (event.target.validity.valueMissing) {
+//     document.querySelector('label[for="firstName"]').style.display = "flex";
+//   }
+// });
 
-inputPassword.addEventListener("invalid", function (event) {
-  if (event.target.validity.valueMissing) {
-    document.querySelector('label[for="password"]').style.display = "flex";
-  }
-});
+// inputFirstName.addEventListener("change", function (event) {
+//   document.querySelector("label[for='firstName']").style.display = "none";
+// });
 
-inputPassword.addEventListener("change", function (event) {
-  document.querySelector("label[for='password']").style.display = "none";
-});
+// inputLastName.addEventListener("invalid", function (event) {
+//   if (event.target.validity.valueMissing) {
+//     document.querySelector('label[for="lastName"]').style.display = "flex";
+//   }
+// });
+
+// inputLastName.addEventListener("change", function (event) {
+//   document.querySelector("label[for='lastName']").style.display = "none";
+// });
+
+// inputEmail.addEventListener("invalid", function (event) {
+//   if (event.target.validity.valueMissing) {
+//     document.querySelector('label[for="email"]').style.display = "flex";
+//   }
+// });
+
+// inputEmail.addEventListener("valid", function (event) {
+//   document.querySelector("label[for='email']").style.display = "none";
+// });
+
+// inputPassword.addEventListener("invalid", function (event) {
+//   if (event.target.validity.valueMissing) {
+//     document.querySelector('label[for="password"]').style.display = "flex";
+//   }
+// });
+
+// inputPassword.addEventListener("change", function (event) {
+//   document.querySelector("label[for='password']").style.display = "none";
+// });
